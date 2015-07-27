249c87f7ce491c02341c8c3b3487d3ccc263ccc2
Simon Cho <simon.cho2093@gmail.com>
2015-07-27 15:04:50 -0700

    uploaded picture

--- GAngsters/config/routes.rb
+++ GAngsters/config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
