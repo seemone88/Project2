a9b333e21d8a9b946b0d28b4783d363929b2d0ad
Simon Cho <simon.cho2093@gmail.com>
2015-07-27 19:09:39 -0700

    new rails, users, sessions

--- GAngsters/app/controllers/users_controller.rb
+++ GAngsters/app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new user_params
+    if @user.save
+      redirect_to users_path
+    else
+      render 'new'
+  end
+end
+
+  def show
+    @users = User.all
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :birthday)
   end
 end
--- GAngsters/config/routes.rb
+++ GAngsters/config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
-  get 'users/index'
-
-  get 'users/new'
-
-  get 'users/create'
+  root to: "users#new"
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
